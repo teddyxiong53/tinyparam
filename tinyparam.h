54bfd0a1639aca66a6de8d565d384937b870d473
teddyxiong53 <1073167306@qq.com>
2025-07-16 17:04:50 +0800

    Update tinyparam.h

--- tinyparam.h
+++ tinyparam.h
@@ -1,11 +1,4 @@
-/*
-* Copyright (c) 2025 Amlogic, Inc. All rights reserved.
-*
-* This source code is subject to the terms and conditions defined in the
-* file 'LICENSE' which is part of this source code package.
-*
-*
-*/
+
 #ifndef __TINYPARAM_H__
 #define __TINYPARAM_H__
 
